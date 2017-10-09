e68fddeccdd887e36db21e68d824edadaafd3409
Victor Yacovlev <v.yacovlev@gmail.com>
2017-10-09 16:37:48 +0300

    Mesheryakov

--- mesheryakov/build/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "server.h"
-
-int main(int argc, char *argv[]) {
-    if (argc < 3) {
-      std::cerr << "Usage: ./start_server port root_dir" << std::endl;
-      return 1;
-    }
-    start_listening(std::stoi(argv[1]), argv[2]);
-}
--- mesheryakov/build/server.h
+++
@@ -1,8 +0,0 @@
-#include "service.h"
-
-#include <arpa/inet.h>
-#include <unistd.h>
-
-void start_listening(int port, const char* htdocs_dir);
-void process_connection(int client_socket, const struct sockaddr_in *client_address, const char* htdocs_dir);
-void interact_connection(int client_socket, const char* client_ip, const uint16_t client_port, const char* htdocs_dir);
